c4e0495b4d6f055d1e25a73e23e220975aa7a203
shavla <rakviashvili95@gmail.com>
2025-09-24 10:09:23 +0400

    init front

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+    <div className="text-3xl font-bold underline text-blue-500">
+      Tailwind is working!
+    </div>
+    </>
+  )
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Client } from 'pg';
 
 dotenv.config();
 
@@ -13,8 +14,17 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('API is runnssing');
+  res.send('API is runnassing');
 });
+
+// Define your PostgreSQL connection configuration
+// const client = new Client({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT,
+// });
 
 // Start server
 app.listen(PORT, () => {
